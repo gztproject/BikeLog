c92fa3728ff3ab3062557d49ae5c102b0044372e
GZTproject <gasper.doljak@gmail.com>
2021-05-22 13:12:49 +0200

    Bugfix, added update script

---
+++ Update.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+# Pull master branch
+git reset --hard
+git pull origin master
+
+#install new dependencies
+rm -rf vendor/
+rm -rf node-modules/
+ 
+composer install --no-dev --optimize-autoloader
+
+php ./bin/console cache:clear --env=prod
+
+#set Environment variables
+composer dump-env prod
+
+#Build new .js and .css
+yarn install
+yarn encore prod
+
+#Migrate DB if necessary
+php bin/console doctrine:migrations:migrate -n
