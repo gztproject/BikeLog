d5dc0cce6115e24da6ac8883159da20580494d0f
GZTproject <gasper.doljak@gmail.com>
2021-08-10 07:38:08 +0200

    Added an update page and js

--- assets/js/admin/update.js
+++ assets/js/admin/update.js
@@ -3,5 +3,6 @@ $('#btn-check-updates').on('click', function (event) {
     $confirm = $('#updateModal');
    
     $('.modal-body').html("Checking for updates...");
+    
     $confirm.modal('show');
 });
