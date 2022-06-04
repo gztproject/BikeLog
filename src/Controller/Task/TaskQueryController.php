15c83efcfe19480e9fe5507c23bf3ace6518d03e
GZTproject <gasper.doljak@gmail.com>
2022-06-04 11:05:53 +0200

    Added some more viewers

--- src/Controller/Task/TaskQueryController.php
+++ src/Controller/Task/TaskQueryController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller\Task;
 
 use App\Entity\Bike\Bike;
@@ -12,29 +11,51 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\Task\TaskRepository;
 
-class TaskQueryController extends AbstractController {
-	/**
-	 *
-	 * @Route("/dashboard/task", methods={"GET"}, name="tasks_index")
-	 */
-	public function index(TaskRepository $tasks, BikeRepository $bikes, Request $request, PaginatorInterface $paginator): Response {
-		$bikeId = $request->query->get ( 'bike', null );
-		$bikeId = $bikeId === "" ? null : $bikeId;
-		$bike = null;
-		if ($bikeId) {
-			$bike = $bikes->findOneBy ( [ 
-					'id' => $bikeId
-			] );
-			if ($bike->getOwner () != $this->getUser ())
-				throw new SecurityError ( "Bikes can only be shown to their owners." );
-		}
+class TaskQueryController extends AbstractController
+{
 
-		$queryBuilder = $tasks->getBikeTasks( $bike->getId()->toString(), $bike->getModel()->getId()->toString());
+    /**
+     *
+     * @Route("/{area}/task", methods={"GET"}, name="tasks_index")
+     */
+    public function index(TaskRepository $tasks, BikeRepository $bikes, Request $request, PaginatorInterface $paginator, $area = "dashboard"): Response
+    {
+        $bikeId = $request->query->get('bike', null);
+        $bikeId = $bikeId === "" ? null : $bikeId;
+        $bike = null;
+        if ($bikeId) {
+            $bike = $bikes->findOneBy([
+                'id' => $bikeId
+            ]);
+            if ($bike->getOwner() != $this->getUser())
+                throw new SecurityError("Bikes can only be shown to their owners.");
+        }
 
-		$pagination = $paginator->paginate ( $queryBuilder, $request->query->getInt ( 'page', 1 ), 10 );
-		return $this->render ( 'dashboard/task/index.html.twig', [ 
-				'tasks' => $pagination,
-				'bike' => $bike
-		] );
-	}
+        $myTasksDto = [];
+        if ($bike != null)
+            $myTasksDto[] = [
+                "tasks" => $tasks->getBikeTasks($bike->getId()
+                    ->toString(), $bike->getModel()
+                    ->getId()
+                    ->toString()),
+                "bike" => $bike
+            ];
+        else if ($area == "admin") {
+            foreach ($bikes->findAll() as $curBike) {
+                $myTasksDto[] = [
+                    "tasks" => $tasks->getBikeTasks($curBike->getId()
+                        ->toString(), $curBike->getModel()
+                        ->getId()
+                        ->toString()),
+                    "bike" => $curBike
+                ];
+            }
+        }
+
+        $pagination = $paginator->paginate($myTasksDto, $request->query->getInt('page', 1), 10);
+        return $this->render('dashboard/task/index.html.twig', [
+            'tasksDto' => $pagination,            
+            'area' => $area
+        ]);
+    }
 }
